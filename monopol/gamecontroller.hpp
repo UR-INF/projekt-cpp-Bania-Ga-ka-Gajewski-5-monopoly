c322e4b3efc77a1855241b027bb8c81e3b60ffb4
Kamil Bania <kamilbania2@gmail.com>
2019-10-26 16:19:31 +0200

    First version of GameController declaration

--- monopol/gamecontroller.hpp
+++ monopol/gamecontroller.hpp
@@ -1,4 +1,6 @@
 #include "player.hpp"
+#include "diceroller.hpp"
+#include <vector>
 
 #ifndef GAMECONTROLLER_H
 #define GAMECONTROLLER_H
@@ -6,12 +8,25 @@
 class GameController {
     private:
         int board; // tu bedzie obiekt planszy
-        Player players[];
+        DiceRoller* diceRoller;
+        int numberOfPlayers;
+        int numberOfActivePlayers; // liczba graczy która jest w rozgrywce, nie-bankruci
+        std::vector<Player> players;
+        Player* currentPlayer;
+        int playerPositions[];
+
+
     
     public:
         GameController();
         ~GameController();
-        
+        void start();
+        void setPlayersOnStart();
+        void nextPlayer();
+        bool doesSomeoneWin();
+        void performAction();
+        std::vector<Player> setPlayersOrder(); 
+
 };
 
 #endif
