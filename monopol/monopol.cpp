9697ed29659c717a6df17c1df2e475b5f74349f6
Kamil Bania <kamilbania2@gmail.com>
2019-10-27 21:25:30 +0100

    Deletion of commented code, add the right game instructions

--- monopol/monopol.cpp
+++ monopol/monopol.cpp
@@ -1,116 +1,49 @@
 ﻿// monopol.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
 //
 #include <iostream>
+#include <string>
 #include <time.h>       /* time */
 #include "player.hpp"
 #include "diceroller.hpp"
+#include "gamecontroller.hpp"
+
 
 using namespace std;
 
-/*
-class Field {
-	int id;
-	int country;
-	int field_owner;
-	int price_of_field;
-	int rent_price_tier1;
-	int rent_price_tier2;
-	int rent_price_tier3;
-	int rent_price_tier4;
-	int home_count;
-
-public:
-	int get_id() { return id; }
-	void set_id(int id) {
-		this->id = id;
-	}
-
-	int get_country() { return country; }
-	void set_country(int country) {
-		this->country = country;
-	}
-
-	int get_field_owner() { return field_owner; }
-	void set_field_owner(int field_owner) {
-		this->field_owner = field_owner;
-	}
-
-	int get_price_of_field() { return price_of_field; }
-	void set_price_of_field(int price_of_field) {
-		this->price_of_field = price_of_field;
-	}
-
-	int get_rent_price_tier1() { return rent_price_tier1; }
-	void set_rent_price_tier1(int rent_price_tier1) {
-		this->rent_price_tier1 = rent_price_tier1;
-	}
-
-	int get_rent_price_tier2() { return rent_price_tier2; }
-	void set_rent_price_tier2(int rent_price_tier2) {
-		this->rent_price_tier2 = rent_price_tier2;
-	}
-
-	int get_rent_price_tier3() { return rent_price_tier3; }
-	void set_rent_price_tier3(int rent_price_tier3) {
-		this->rent_price_tier3= rent_price_tier3;
-	}
-
-	int get_rent_price_tier4() { return rent_price_tier4; }
-	void set_rent_price_tier4(int rent_price_tier4) {
-		this->rent_price_tier4 = rent_price_tier4;
-	}
-
-	int get_home_count() { return home_count; }
-	void set_home_count(int home_count) {
-		this->home_count = home_count;
-	}
-
-
-};
-*/
-
-// void chance_card() {
-
-// 	static int move_pawn = 20;	/* - przesunięcie pionka						*/
-// 	static int payment = 40;	/* - wpłata pieniędzy do banku					*/
-// 	static int withdrawal = 60; /* - otrzymanie pieniędzy						*/
-// 	static int jail = 80;		/* - konieczność pójścia do więzienia			*/
-// 	static int jail_card = 100; /* - możliwość bezpłatnego wyjścia z więzienia  */
-
-// 	int random_card = rand() % 101;
-
-// 	if (random_card <= move_pawn) {
-// 		std::cout << "przenieś pionek\n";
-// 	}
-// 	else if (random_card <= payment) {
-// 		std::cout << "płacisz x pieniedzy\n";
-// 	}
-// 	else if (random_card <= withdrawal) {
-// 		std::cout << "otrzymujesz x pieniedzy\n";
-// 	}
-// 	else if (random_card <= jail) {
-// 		std::cout << "idziesz do więzienia\n";
-// 	}
-// 	else if (random_card <= jail_card) {
-// 		std::cout << "dostajesz karte wyjscia z wiezienia\n";
-// 	}
-
-// }
-
 int main()
 {
-	PlayerState* ps = new PlayerState(1500, false);
-	Player* player = new Player("Janusz", ps, 0);
+	char numOfPlayersChar = '0';
+	int numOfPlayers = 0;
 
-	cout << player->getName() << endl;
+	while(true) {
+		cout << "Podaj ilosc graczy ktora bedzie grac: " << endl;
+		cin >> numOfPlayersChar;
 
-	DiceRoller* dr = new DiceRoller();
-	DiceThrowResult* dtr = dr->rollDices();
+		if (!isdigit(numOfPlayersChar)) {
+			cout << "Nie podano cyfry!" << endl;
+			continue;			
+		}
 
-	cout << "Wynik z pierwszej kostki: " << dtr->firstDice << endl;
-	cout << "Wynik z drugiej kostki: " << dtr->secondDice << endl;
-	cout << "Czy wyrzucono debel: " << dtr->isDouble() << endl;
-	
+		numOfPlayers = (int) numOfPlayersChar;
+		numOfPlayers = numOfPlayers - 48;
+
+		if ((numOfPlayers > 1) && (numOfPlayers < 5)) {
+			cout << "OK! W grze wezmie udzial " << numOfPlayers << " graczy" << endl;
+			break;
+		}
+		else {
+			cout << "Liczba musi być z przedzialu 2-4" << endl;
+		}
+	}
+
+	Board* board = new Board();
+	DiceRoller* diceRoller = new DiceRoller();
+
+	GameController* gameController = new GameController(board, diceRoller, numOfPlayers);
+
+	gameController->start();
+
+	cin >> numOfPlayersChar;
 	getchar();
     
 }
