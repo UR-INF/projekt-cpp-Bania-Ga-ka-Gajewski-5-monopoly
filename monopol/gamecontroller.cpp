4a25fabfb7d9e8e4433760cf95f0fdce3fe370c0
Kamil Bania <kamilbania2@gmail.com>
2019-12-03 19:06:29 +0100

    PickBlueCard commented on start

--- monopol/gamecontroller.cpp
+++ monopol/gamecontroller.cpp
@@ -51,7 +51,7 @@ void GameController::start() {
     this->setPlayersOnStart();
 
     bool isPlaying = true;
-	pickBlueCard(currentPlayer);
+	// pickBlueCard(currentPlayer);
     while(isPlaying && this->numberOfActivePlayers > 1) {
         
         this->renderCurrentPlayer();
